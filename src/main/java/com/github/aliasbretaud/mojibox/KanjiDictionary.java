ebfafb282d518bb6aa221430860232d10610bca4
Florian Bretaud <lcdopg@gmail.com>
2024-04-10 19:46:29 +0900

    restruct KanjiDictionary

--- src/main/java/com/github/aliasbretaud/mojibox/KanjiDictionary.java
+++ src/main/java/com/github/aliasbretaud/mojibox/KanjiDictionary.java
@@ -1,27 +1,19 @@
 package com.github.aliasbretaud.mojibox;
 
-import lombok.Getter;
-
 import java.io.IOException;
-import java.util.Map;
 
 import static java.lang.Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS;
 
 public class KanjiDictionary {
 
-    @Getter
-    private final Metadata metadata;
-
-    private final Map<String, Kanji> kanjis;
+    private final DictionaryData data;
 
     public KanjiDictionary() {
         var dictionaryUrl = getClass()
                 .getClassLoader()
                 .getResource("kanji_dictionary.json");
         try {
-            var data = new DictionaryBuilder(dictionaryUrl).generateData();
-            this.metadata = data.getMetadata();
-            this.kanjis = data.getKanjis();
+            this.data = new DictionaryBuilder(dictionaryUrl).generateData();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -32,10 +24,14 @@ public class KanjiDictionary {
                 Character.UnicodeBlock.of(search) != CJK_UNIFIED_IDEOGRAPHS) {
             return null;
         }
-        return this.kanjis.get(String.valueOf(search));
+        return this.data.getKanjis().get(String.valueOf(search));
+    }
+
+    public Metadata getMetadata() {
+        return this.data.getMetadata();
     }
 
     public int getSize() {
-        return this.kanjis.size();
+        return this.data.getKanjis().size();
     }
 }
